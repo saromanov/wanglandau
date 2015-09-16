308ac8fe5cf42b81cf4dfd2e36551a4ed6b24785
Sergey Romanov <xxsmotur@gmail.com>
2015-09-17 01:23:06 +0500

    Add find_neighbours

--- src/wanglandau.js
+++ src/wanglandau.js
@@ -5,7 +5,7 @@ class WangLandau {
     constructor(dimensions=2) {
         this.dimensions = dimensions;
         this.critical = 0;
-        this.modules = 0;
+        this.modules = 1;
         this.neighbours = [];
         this.lattice = [];
     }
@@ -24,5 +24,12 @@ class WangLandau {
             result += this.lattice[this.neighbours[i]];
         }
         return result;
+    }
+    
+    find_neighbours(n) {
+        for(let i = 0;i < this.dimensions;++i) {
+            this.neighbours[2*i] = (n + this.reach[i] + this.modules) % this.modules;
+            this.neighbours[2*i+1] = (n - this.reach[i] +this.modulus) % this.modulus;
+        }
     }
 }
